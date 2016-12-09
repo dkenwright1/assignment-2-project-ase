f4b59c7ec5af10c94c6a37ce4b93ecf107aab8a1
c3443623 <c3443623@leedsbeckett>
2016-12-09 11:58:11 +0000

    added code which clears text boxes when the clear button is clicked and login verification code that is connected to a database.

--- assignment 2 project ase/home.aspx.cs
+++ assignment 2 project ase/home.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +17,66 @@ namespace assignment_2_project_ase
 
         protected void clearbtn_Click(object sender, EventArgs e)
         {
+            user.Text = String.Empty;
+            txtpass.Text = String.Empty;
+        }
 
+        protected void loginbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Data.OleDb.OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\ASD\ase 2\user Database.accdb");
+                connection.Open();
+                OleDbCommand command2 = new OleDbCommand("select * from admin where username='" + user.Text + "' and [password]='" + txtpass.Text + "'", connection);
+                OleDbDataReader reader = command2.ExecuteReader();
+                if (reader.Read() == true)
+                {
+                    Response.Write("admin signed in");
+                    //new admin().Show();
+                    //this.Hide();
+
+
+                    // break;
+                }
+                else
+                {
+
+                    OleDbCommand command = new OleDbCommand("select * from users where username='" + user.Text + "' and [password]='" + txtpass.Text + "'", connection);
+                    OleDbDataReader reader2 = command.ExecuteReader();
+
+
+
+
+
+
+                    if (reader2.Read() == true)
+                    {
+                        //Session ["username"] = usernametxt.Text;
+
+                        Response.Write("signed in as user");
+                        //this.Hide();
+                        //user userpage = new user(usernametxt.Text);
+                        //userpage.ShowDialog();
+
+
+
+
+
+
+
+                    }
+                    else
+                    {
+                        Response.Write("account doesn't exist");
+                        //   break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Failed due to " + ex.Message);
+
+            }
         }
     }
 }
