41ff3291ace51c49f368470d28ac6b643337fe5b
dkenwright1 <dkenwright1@gmail.com>
2017-01-09 03:45:39 +0000

    Add files via upload
    added a testing class which tests various queries, all these tests pass.

---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using logon;
+
+
+namespace tracker_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void connection_test()
+        {
+            using (var connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ben\Documents\myDB.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                try
+                {
+                    connection.Open();
+                    connection.Close();
+                    Assert.IsTrue(true);
+                }
+                catch (SqlException)
+                {
+                    Assert.IsTrue(false);
+                }
+            }
+        }
+    }
+}
